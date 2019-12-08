138bfa79e677634b2473cbbb161a6bc3f88ebce2
LeeTrent <lee.trent.1@gmail.com>
2019-12-08 11:59:01 -0600

    Builds user interface for TasksScreen (mostly complete)

---
+++ android/app/src/main/java/trenttek/com/todoey_flutter/MainActivity.java
@@ -0,0 +1,13 @@
+package trenttek.com.todoey_flutter;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
