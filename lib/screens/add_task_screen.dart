1cb8837c4434572445a5afd29b4a45c8254d7a38
LeeTrent <lee.trent.1@gmail.com>
2019-12-10 21:02:07 -0600

    Partially completes add task screen

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -6,12 +6,32 @@ class AddTaskScreen extends StatelessWidget {
     return Container(
       color: Color(0xFF757575),
       child: Container(
+        padding: EdgeInsets.only(
+          top: 20.0,
+        ),
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.only(
             topLeft: Radius.circular(20.0),
             topRight: Radius.circular(20.0),
           ),
+        ),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Add Task',
+              style: TextStyle(
+                color: Colors.lightBlueAccent,
+                fontSize: 30.0,
+              ),
+            ),
+            FlatButton(
+              padding: EdgeInsets.all(8.0),
+              color: Colors.lightBlueAccent,
+              textColor: Colors.red,
+              child: Text('Add'),
+            ),
+          ],
         ),
       ),
     );
